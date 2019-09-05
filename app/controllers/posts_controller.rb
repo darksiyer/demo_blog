531c70a9ef813351b2974d77ca30116b086d044f
darksiyer <moises.chin1@gmail.com>
2019-09-05 13:44:43 -0500

    Boton de borrado funcional para los posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,6 +39,13 @@ class PostsController < ApplicationController
 
 	end
 
+	def destroy 
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
 	private
 
 	def post_params
